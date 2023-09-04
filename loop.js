0a868060b4f7b8a5218834d9f00909409e41e13e
Dineshika <91914577+DineshikaSivapalaraja@users.noreply.github.com>
2023-09-04 15:31:12 +0530

    for loop covered

---
+++ loop.js
@@ -0,0 +1,31 @@
+//for loop
+
+// print the name 5 times
+for (var count=1; count<=5; count++){
+    console.log("Din")
+}
+
+//print 1 to 10
+for(var n=1;n<=10;n++){
+    console.log(n)
+}
+
+//print 1 to 10 (only 1,3,5,7,9)
+for(var o=1; o<=10; o=o+2){
+    console.log(o)
+}
+
+//print 1 to 10 in reverse order
+for(i=10;i>=1 && i<=10;i--){
+    console.log(i)
+}
+
+//print even numbers between 1 to 10
+for(n=2;n<=10;n+=2){
+    console.log(n)
+}
+
+//print 2tables
+for(n=1;n<=10;n++){
+    console.log(n+"*2="+(n*2))
+}
